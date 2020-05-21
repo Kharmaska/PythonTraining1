d6fb54198f67a95894c9e746b72cfffe429c2167
Vincent Moulin - Kharmaska <kharmachaitea@gmail.com>
2020-05-21 10:59:02 +0200

    while loop tests

--- san_antonio.py
+++ san_antonio.py
@@ -1,34 +1,13 @@
-quotes = [
-    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", 
-    "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
-]
+quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", "On doit pouvoir choisir entre s'écouter parler et se faire entendre."]
 
-characters = [
-    "alvin et les Chipmunks", 
-    "Babar", 
-    "betty boop", 
-    "calimero", 
-    "casper", 
-    "le chat potté", 
-    "Kirikou"
-]
-user_answer = input('Tapez entrée pour découvrir une autre citation ou B pour quitter le programme.')
+characters = ["alvin et les Chipmunks", "Babar", "betty boop", "calimero", "casper", "le chat potté", "Kirikou"]
 
-# Show random quote
-def get_random_quote(my_list):
-	item = my_list[0]
-	return item
+def get_random_item_in(my_list):
+    # get a random number
+    item = my_list[0] # get a quote from a list
+    return item # return the item
 
-print (get_random_quote(my_list))
+user_answer = "A"
 
-
-if user_answer == "B":
-    pass
-elif user_answer == "C":
-    print("C pas la bonne réponse ! Et G pas d’humour, je C...")
-else:
-	pass
-    # show another quote
-
-def show_random_quote():
-	pass
+while user_answer != "B":
+    print(get_random_item_in(quotes))
